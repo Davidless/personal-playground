d2c78aa2a26925c93e16e346ee1e4568d2270302
David Alexander Morales <damorales89@gmail.com>
2018-11-12 18:21:05 -0500

    Changed font stylings for landing page text

--- playground/src/Components/Landing.js
+++ playground/src/Components/Landing.js
@@ -11,6 +11,12 @@ export default class Landing extends Component {
         </span>
         <p className="LandingBlurb">
           I manipulate ideas for screens and design seamless user experiences.
+          When I'm not brainstorming new concepts, I like to challenge myself
+          with bodyweight exercises and enjoy zoning out to an eclectic mix of
+          sounds.
+          <br />
+          <br />
+          Project page under construction
         </p>
         <div className="LandingButtons">
           <button type="button" disabled className="Project-Button">
