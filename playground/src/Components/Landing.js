4b85e111e4d83724fadcdbdc82364e699be7f00b
David Alexander Morales <damorales89@gmail.com>
2018-11-12 17:56:37 -0500

    Changed button to disabled until I finish project page

--- playground/src/Components/Landing.js
+++ playground/src/Components/Landing.js
@@ -7,11 +7,15 @@ export default class Landing extends Component {
       <div className="Landing">
         <h1 className="LandingName">David Alexander Morales</h1>
         <span className="LandingTitle">
-          UX/UI Designer | Full Stack Engineer | Calisthenics Enthusiast
+          UX/UI Designer | Full Stack Engineer
         </span>
-        <p className="LandingBlurb">I manipulate ideas for screens. I </p>
+        <p className="LandingBlurb">
+          I manipulate ideas for screens and design seamless user experiences.
+        </p>
         <div className="LandingButtons">
-          <button type="button" className="Project-Button">View Projects</button>
+          <button type="button" disabled className="Project-Button">
+            View Projects
+          </button>
           {/* <button className="Project-Contact">Contact Me</button> */}
         </div>
       </div>
